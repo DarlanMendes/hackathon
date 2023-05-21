5407e62fd202b4698b23d83d0847e8f9df084b96
Darlan Mendes <dedsondarlan@gmail.com>
2023-05-21 20:58:59 -0300

    Ajuste next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
-
+import { ThemeProvider } from '../context/theme';
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return <SessionProvider session={session}>
+    <ThemeProvider>
     <Component {...pageProps} />
+    </ThemeProvider>
   </SessionProvider>
 
 }
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,10 +1,35 @@
+import Header from '@/components/Header'
+import Navsidebar from '@/components/Navsidebar'
 import { getSession, signOut } from 'next-auth/react'
-export default function Dashboard() {
+import { useTheme } from '../../context/theme';
+interface Props {
+    user: {
+        name: string,
+        email: string,
+        image: string
+    }
+}
+export default function Dashboard(props: Props) {
+    const { theme, toggleTheme } = useTheme();
     return (
-        <div>
-            <main>
-                Dashboard
-                <button onClick={() => signOut()}>Sign Out </button>
+        <div className='grid' style={{ gridTemplateColumns: "2fr 10fr" }}>
+            <Navsidebar 
+            theme={theme}
+            toggleTheme={toggleTheme}
+            />
+
+
+
+            <main className="bg-zinc-100 ">
+                <Header theme={theme} user={props.user} />
+                <div className=" pt-11 pl-64">
+                    <h1 className="text-4xl font-bold">
+                        Dashboard
+                    </h1>
+
+                    <button onClick={() => signOut()}>Sign Out </button>
+                </div>
+
             </main>
         </div>
 
@@ -28,13 +53,13 @@ export async function getServerSideProps(ctx: any) {
                 }
             }
         }
-    }else{
-        return{
+    } else {
+        return {
             redirect: {
                 destination: '/login',
                 permanent: false
             }
         }
-        
+
     }
 }   
