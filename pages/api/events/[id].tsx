b083970295f6d2b6dcd4b4684ca8b4ea647b73c2
Darlan Mendes <dedsondarlan@gmail.com>
2023-05-22 08:13:07 -0300

    Ajustes das rotas de api

--- pages/api/events/[id].tsx
+++ pages/api/events/[id].tsx
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
-
+import {Event} from "@prisma/client"
 const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
 
 type Data = {
-  name: string
+  erro: string
 }
 
 export default async  function handler(
@@ -20,7 +20,7 @@ export default async  function handler(
         const event = await prisma.event.findUnique({where:{id:id}})
         console.log(event)
         if(event){
-            res.send({event:event})
+            res.send(event)
         }
         else{
             res.send({erro:"Usuário não encontrado"})
--- pages/api/rooms/[id].tsx
+++ pages/api/rooms/[id].tsx
@@ -1,26 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
-
+import {Room} from "@prisma/client"
 const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
 
 type Data = {
-  name: string
+  erro: string
 }
 
 export default async  function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data|Room>
 ) {
     
    const {id} =  req.query
-    if(typeof id ===  'string'){
+    if(typeof id ===  'number'){
         
         const room = await prisma.room.findUnique({where:{id:id}})
         console.log(room)
         if(room){
-            res.send({room:room})
+            res.send(room)
         }
         else{
             res.send({erro:"Usuário não encontrado"})
