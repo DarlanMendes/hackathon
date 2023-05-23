9c6582cf6066e90588832982fffb6a9f6ff434bf
Darlan Mendes <dedsondarlan@gmail.com>
2023-05-23 03:47:54 -0300

    CRUD rooms

---
+++ pages/api/rooms/index.tsx
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+import {Room} from "@prisma/client"
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+type Data = {
+  erro: string
+}
+
+export default async  function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data|Room|Array<Room>>
+) {
+    
+   if(req.method==='GET'){
+
+    try{
+        const allRooms = await prisma.room.findMany()
+        res.send(allRooms)
+    }catch{
+        res.send({erro:'Erro ao buscar salas'})
+    }
+   }
+   if(req.method ==="POST"){
+    const {name,capacity,resources,photo,eventId } = req.body
+    if(name&&capacity&&resources&&photo&&eventId ){
+        try{
+            const roomCreated = await prisma.room.create({data:{
+                name,capacity,resources,photo,eventId
+            }})
+            return res.send(roomCreated)
+        }catch{
+            return res.send({erro:"Erro ao criar sala"})
+        }
+    }
+   }
+   res.send({erro:"Erro ao acessar"})
+   
+
+
+}
