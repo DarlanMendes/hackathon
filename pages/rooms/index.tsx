57a8393eeb1cd8b7e6da33436cde2e35d6526d9f
Darlan Mendes <dedsondarlan@gmail.com>
2023-05-23 15:30:38 -0300

    Crição página rota users rooms

---
+++ pages/rooms/index.tsx
@@ -0,0 +1,14 @@
+export default function Rooms(){
+
+    //Neste espaço desenvolver as regras de negócio
+ 
+    return(
+        <div>
+
+            Rooms 
+
+
+            {/* Listar as salas disponveis  */}
+        </div>
+    )
+}
